98b93908df124479d8a2026c0d3a60c5f5d40e71
Minh Doan Phan Cong <minhdpc@gmail.com>
2022-06-30 23:47:36 +0700

    Rust build program
    1. Create simple function call last name and first name
    2. Build the first program with "cargo" command

---
+++ src/bin/a1.rs
@@ -0,0 +1,15 @@
+// Using function to display first name
+fn first_name() {
+    println!("Minh")
+}
+
+// Using function to display last name
+fn last_name() {
+    println!("Doan")
+}
+
+// Using println to display message in main function
+fn main() {
+    first_name();
+    last_name();
+}
